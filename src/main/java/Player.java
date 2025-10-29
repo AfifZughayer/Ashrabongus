f1eade9c4644c63eaaf14c0eab16dfbe634f7d33
AfifZughayer <aaamz2004@gmail.com>
2025-10-29 15:22:25 +0400

    stuff

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -3,19 +3,33 @@ import godot.annotation.RegisterClass;
 import godot.annotation.RegisterFunction;
 import godot.annotation.RegisterProperty;
 import godot.api.*;
+import godot.core.Vector3;
+
+import java.util.Random;
 
 @RegisterClass
 public class Player extends CharacterBody3D {
 
     @Export
     @RegisterProperty
-    public int speed = 5;
+    public float speed = 5f;
     @Export
     @RegisterProperty
     public float sens = 0.005f;
     @Export
     @RegisterProperty
+    public float fireRate = 1;
+    @RegisterProperty
+    public float timer = 0;
+
+    @RegisterProperty
     public Camera3D cam;
+    @Export
+    @RegisterProperty
+    public PackedScene projectile;
+    @Export
+    @RegisterProperty
+    public Marker3D gunPos;
 
     @RegisterFunction
     public void _ready(){
@@ -25,9 +39,33 @@ public class Player extends CharacterBody3D {
 
     @RegisterFunction
     public void _input(InputEvent event){
+        assert event != null;
+        if (event.isActionPressed("Space")){
+            speed = 0f;
+        }
+        if (event.isActionReleased("Space")){
+            speed = 5f;
+        }
         if (event instanceof InputEventMouseMotion mouseEvent){
             rotateY((float) -mouseEvent.getRelative().getX() * sens);
             cam.rotateX((float) mouseEvent.getRelative().getY() * sens);
+        }
+    }
+
+    @RegisterFunction
+    public void _process(double delta) {
+        if (timer > 0){
+            timer -= delta;
+        }
+        if (Input.isActionPressed("Shoot") && timer <= 0) {
+            Node3D proj_instance = (Node3D) projectile.instantiate();
+            getTree().getCurrentScene().addChild(proj_instance);
+            Random rand = new Random();
+            float mix = ((rand.nextFloat()*2)-1) * 0.05f;
+            Vector3 pos = new Vector3(gunPos.getGlobalPosition().getX() + mix, gunPos.getGlobalPosition().getY() + mix, gunPos.getGlobalPosition().getZ() + mix);
+            proj_instance.setGlobalPosition(pos);
+            proj_instance.setGlobalRotation(cam.getGlobalRotation());
+            timer = 1.0f / fireRate;
         }
     }
 
--- src/main/java/Projectile.java
+++ src/main/java/Projectile.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import godot.annotation.Export;
+import godot.annotation.RegisterFunction;
+import godot.annotation.RegisterProperty;
+import godot.annotation.RegisterClass;
+import godot.api.Area3D;
+import godot.core.*;
 
-public class Projectile {
+@RegisterClass
+public class Projectile extends Area3D {
+
+    @Export
+    @RegisterProperty
+    public float dmg;
+
+    @Export
+    @RegisterProperty
+    public float speed = 5;
+
+    @RegisterProperty
+    public float timer = 0;
+
+    @RegisterFunction
+    public void dealDamage(Area3D area) {
+        if (area.isInGroup("damageable")){
+            area.queueFree();
+        }
+    }
+
+    @RegisterFunction
+    public void _process(double delta){
+        if (timer >= 10){
+            queueFree();
+        }
+        timer += delta;
+
+        Vector3 pos = getPosition().plus(getBasis().getZ().times(-speed * delta));
+        setPosition(pos);
+    }
+
 }
